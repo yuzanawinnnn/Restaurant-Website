1524a011e6e12ff08b312a7ccabdacd186ef5fd9
Yuzana <56732512+yuzanawinnnn@users.noreply.github.com>
2023-11-20 06:53:56 +0630

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,19 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 
-//     section.forEach(sec =>{
-//       let top =
-//     });
+     section.forEach(sec =>{
+       let top = window.scrollY;
+       let height = sec.offsetHeight - 150;
+       let offset = sec.offsetTop - 150;
+       let id = sec.getAttribute('id');
+
+       if(top => offset && top < offset + height){
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelectorAll('header .navbar a[href='+id+']').classList.add('active');
+        });
+       }
+    });
  }
 
 document.querySelector("#search-icon").onclick = () =>{
